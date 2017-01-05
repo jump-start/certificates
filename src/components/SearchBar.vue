6c20735a6042d61308f4310f1e198906d26eeb5a
Kevin <kevinlee438@hotmail.co.uk>
2017-01-05 13:01:15 +0000

    Added click handler for search

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,10 +1,26 @@
 <template>
   <div class="ui transparent icon input">
-    <input type="text" placeholder="Search...">
-    <i class="search link icon"></i>
+    <input id="search-bar" v-model="searchTerm" type="text" placeholder="Search...">
+    <i v-on:click="search()" class="search link icon"></i>
   </div>
 </template>
 
 <script>
-  export default {};
+  export default {
+    data() {
+      return {
+        searchTerm: ''
+      };
+    },
+    methods: {
+      search() {
+        return [
+          {
+            title: 'Certificates',
+            description: 'Lorem ipsum'
+          }
+        ];
+      }
+    }
+  };
 </script>
